65ff1ef231f04800a61a6c7eeb4fe386475a8a44
Kkun84 <kesuike84@gmail.com>
2019-06-26 17:07:31 +0900

    [fix]make('SquareField-v0')できない問題を修正
    __init__.pyからenvs.pyへの相対パスを与えていたのをsys.pathに含まれるpathからの相対パスに変更

--- mylib/mygym/__init__.py
+++ mylib/mygym/__init__.py
@@ -2,10 +2,22 @@ from .mygym import Transition, History, ReplayMemory, Epsilon
 from . import wrappers
 from . import envs
 
-from gym.envs.registration import register
+import os
+import sys
 
 
-register(
-    id='SquareField-v0',
-    entry_point='envs:SquareField',
-)
+path = os.path.dirname(__file__) + '/envs'
+for p in sys.path:
+    if not (p and path.startswith(p)):
+        continue
+    path = path[len(p) + 1:].replace('/', '.')
+
+    from gym.envs.registration import register
+
+    register(
+        id='SquareField-v0',
+        entry_point=path + ':SquareField'
+    )
+
+    del register
+    break
