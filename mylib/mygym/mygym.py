b72a23d5887324b7a67e2f8207a000d664ba0f01
Kkun84 <kesuike84@gmail.com>
2019-07-03 22:50:07 +0900

    [minimul]Epsilonの仕様変更
    Epsilonの使い方を変えた

--- mylib/mygym/mygym.py
+++ mylib/mygym/mygym.py
@@ -108,7 +108,13 @@ class Epsilon:
         self.start = start
         self.end = end
         self.decay = decay
+        self.step = 0
 
-    def __call__(self, x):
-        epsilon = self.end + (self.start - self.end) * 2**(-x / self.decay)
+    def __call__(self):
+        epsilon = self.end + (
+            self.start - self.end) * 2**(-self.step / self.decay)
+        self.step += 1
         return epsilon
+
+    def reset(self, step=0):
+        self.step = step
