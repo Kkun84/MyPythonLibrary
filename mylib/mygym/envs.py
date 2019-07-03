6274f59cc7e7e4ffabd11eeca49521d807d8b7fd
Kkun84 <kesuike84@gmail.com>
2019-07-03 15:12:59 +0900

    [minimul]不必要なアンダーバー2つを1つに

--- mylib/mygym/envs.py
+++ mylib/mygym/envs.py
@@ -11,45 +11,45 @@ class SquareField(gym.Env):
         if 'kwargs' in kwargs:
             kwargs = kwargs['kwargs']
 
-        self.__size = kwargs['size'] if 'size' in kwargs else size
-        self.__render_size = kwargs['render_size'] if 'render_size' in kwargs else render_size
-        self.__seed = None
-        self.__position = None
+        self._size = kwargs['size'] if 'size' in kwargs else size
+        self._render_size = kwargs['render_size'] if 'render_size' in kwargs else render_size
+        self._seed = None
+        self._position = None
 
         self.action_space = gym.spaces.Discrete(4)
         self.observation_space = gym.spaces.Discrete(2)
         self.reward_range = [-1.0, 1.0]
 
-    def __observe(self):
-        return self.__position.copy()
+    def _observe(self):
+        return self._position.copy()
 
-    def __is_done(self):
-        return (self.__position < 0).any() or (self.__size <= self.__position).any()
+    def _is_done(self):
+        return (self._position < 0).any() or (self._size <= self._position).any()
 
-    def __is_in_field(self):
-        return (-1 <= self.__position).all() and (self.__position <= self.__size).all()
+    def _is_in_field(self):
+        return (-1 <= self._position).all() and (self._position <= self._size).all()
 
-    def __get_reward(self):
-        return 1.0 if not self.__is_done() else -1.0
+    def _get_reward(self):
+        return 1.0 if not self._is_done() else -1.0
 
     def reset(self):
-        self.__position = (
-            np.random.randint(self.__size, size=2) if self.__seed is None else
-            np.array(divmod(self.__seed, self.__size), dtype=int)
+        self._position = (
+            np.random.randint(self._size, size=2) if self._seed is None else
+            np.array(divmod(self._seed, self._size), dtype=int)
         )
-        observation = self.__observe()
+        observation = self._observe()
         return observation
 
     def seed(self, seed=None):
         if not isinstance(seed, int):
             raise ValueError('not isinstance(seed, int).')
-        self.__seed = seed
+        self._seed = seed
 
     def step(self, action):
-        self.__position += [[1, 0], [0, -1], [-1, 0], [0, 1]][action]
-        observation = self.__observe()
-        reward = self.__get_reward()
-        done = self.__is_done()
+        self._position += [[1, 0], [0, -1], [-1, 0], [0, 1]][action]
+        observation = self._observe()
+        reward = self._get_reward()
+        done = self._is_done()
         return observation, reward, done, {}
 
     def render(self, mode='human', close=False):
@@ -57,22 +57,22 @@ class SquareField(gym.Env):
             raise ValueError('Sorry, "human" is not yet supported.')
 
         elif mode == 'rgb_array':
-            dst = np.zeros([self.__size + 2] * 2 + [3], dtype=int)
+            dst = np.zeros([self._size + 2] * 2 + [3], dtype=int)
             for i in [0]:
                 dst[[0, -1], :, i] = dst[:, [0, -1], i] = 0x80
-            if self.__is_in_field():
+            if self._is_in_field():
                 for i in [0, 1, 2]:
-                    dst[self.__position[1] + 1, self.__position[0] + 1, i] = 0xff
+                    dst[self._position[1] + 1, self._position[0] + 1, i] = 0xff
 
             for i in range(2):
-                dst = dst.repeat(self.__render_size, axis=i)
+                dst = dst.repeat(self._render_size, axis=i)
 
         elif mode == 'ansi':
-            dst = [['+'] * (self.__size + 2) for i in range(self.__size + 2)]
-            for i, j in itertools.product(range(1, self.__size + 1), repeat=2):
+            dst = [['+'] * (self._size + 2) for i in range(self._size + 2)]
+            for i, j in itertools.product(range(1, self._size + 1), repeat=2):
                 dst[i][j] = '0'
-            if self.__is_in_field():
-                dst[self.__position[1] + 1][self.__position[0] + 1] = '1'
+            if self._is_in_field():
+                dst[self._position[1] + 1][self._position[0] + 1] = '1'
             dst = '\n'.join([''.join(d) for d in dst])
 
         else:
