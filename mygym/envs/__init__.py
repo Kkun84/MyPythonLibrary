04d31a8bf4660ce0f7c3101a1ecf993d9b8a9c55
Kkun84 <kesuike84@gmail.com>
2019-07-13 04:18:00 +0900

    [fix]自作環境を追加するパスを変更
    フォルダ変更時に変え忘れていた

--- mygym/envs/__init__.py
+++ mygym/envs/__init__.py
@@ -9,11 +9,13 @@ def _register_myenvs():
     import sys
     from gym.envs.registration import register
 
-    path = os.path.dirname(__file__) + '/envs'
+    path = os.path.dirname(__file__)
     for p in sys.path:
         if not (p and path.startswith(p)):
             continue
         path = path[len(p) + 1:].replace('/', '.')
+
+        print(path)
 
         register(
             id='SquareField-v0',
