aacc5634f47f2d0fc43411e0ae0384f872b873bf
Kkun84 <kesuike84@gmail.com>
2019-07-13 04:04:24 +0900

    [minimal]_register_myenvs()の実行をenvsに移動
    環境に関することをしているのでenvフォルダの__init__.pyに移動

--- mygym/envs/__init__.py
+++ mygym/envs/__init__.py
@@ -1,2 +1,28 @@
 from .SquareField import SquareField
 from .BoxField import BoxField
+
+
+def _register_myenvs():
+    import os
+    import sys
+    from gym.envs.registration import register
+
+    path = os.path.dirname(__file__) + '/envs'
+    for p in sys.path:
+        if not (p and path.startswith(p)):
+            continue
+        path = path[len(p) + 1:].replace('/', '.')
+
+        register(
+            id='SquareField-v0',
+            entry_point=path + ':SquareField'
+        )
+
+        register(
+            id='BoxField-v0',
+            entry_point=path + ':BoxField'
+        )
+
+
+_register_myenvs()
+del _register_myenvs
