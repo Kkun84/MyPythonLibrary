c0703feaa591eace6a88eeb18d1e36a6bee85e19
Kkun84 <kesuike84@gmail.com>
2019-09-02 16:12:41 +0900

    謎
    どこに差があるのかわからない

--- mylib/time.py
+++ mylib/time.py
@@ -11,9 +11,9 @@ def record_time(func, timezone='Asia/Tokyo'):
     def wrapper(*args, **kwargs):
         start_time = get_datetime(timezone)
         print(f'↓--start_time: {start_time}--↓\n')
-        
+
         retvalue = func(*args, **kwargs)
-        
+
         end_time = get_datetime(timezone)
         diff_time = end_time - start_time
         print(f'\n↑--end_time  : {end_time}--↑({diff_time})')
@@ -24,17 +24,17 @@ def record_time(func, timezone='Asia/Tokyo'):
 class MeasureTime():
     def __init__(self):
         self.last = time.time()
-    
+
     def __call__(self):
         t = time.time()
         self.last, dt = t, t - self.last
         return dt
-    
+
 
 if __name__ == "__main__":
     measure_time = MeasureTime()
     print(get_datetime())
-    
+
     measure_time()
     time.sleep(0.1)
     print(measure_time())
